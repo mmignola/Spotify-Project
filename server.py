53d4957f277c7ad3b1afdc0a572c8ed8c043f4b2
Madeleine Mignola <madeleine.mignola@gmail.com>
2023-03-12 19:23:27 -0700

    created fcn to search for artists

--- server.py
+++ server.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template, request, flash, session, redirect
-
-
+from dotenv import load_dotenv
 from jinja2 import StrictUndefined
+import os
+import base64
+from requests import post, get
+import json
+
+load_dotenv()
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
 
 app = Flask(__name__)
 app.secret_key = "dev"
@@ -12,6 +20,74 @@ def homepage():
     """View homepage."""
 
     return render_template("homepage.html")
+
+
+@app.route("/results", methods = ["POST"])
+def search_results():
+    """View search results."""
+
+    search_term = request.form.get("artist")
+    artist_data = search_for_artist(get_token(), search_term)
+
+    return render_template("results.html", artist_data = artist_data)
+
+
+def get_token():
+    """Create authorization string."""
+    auth_string = client_id + ":" + client_secret
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headers = headers, data = data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+
+    return token
+
+
+def get_auth_header(token):
+    return {"Authorization": "Bearer " + token}
+
+
+def search_for_artist(token, artist_name):
+    url = "https://api.spotify.com/v1/search"
+    headers = get_auth_header(token)
+    query = f"?q={artist_name}&type=artist&limit=1"
+
+    query_url = url + query
+    result = get(query_url, headers = headers)
+    json_result = json.loads(result.content)["artists"]["items"]
+
+    if len(json_result) == 0:
+        return None
+    
+    return json_result[0]
+
+
+def get_songs_by_artist(token, artist_id):
+    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
+    headers = get_auth_header(token)
+    result = get(url, headers = headers)
+    json_result = json.loads(result.content)["tracks"]
+
+    return json_result
+
+
+# token = get_token()
+# result = search_for_artist(token, "lovejoy")
+# artist_id = result["id"]
+# songs = get_songs_by_artist(token, artist_id)
+
+# for idx, song in enumerate(songs):
+#     print(f"{idx + 1}. {song['name']}")
+
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
